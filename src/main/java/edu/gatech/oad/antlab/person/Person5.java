fec7f1c8a83f60b9a161905711e6336da9b88ddf
Connie Huang <conniehuang@lawn-128-61-10-45.lawn.gatech.edu>
2017-09-05 20:15:03 -0400

    deleting useless p5 file

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,23 +3,23 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
 public class Person5 {
   /** Holds the persons real name */
   private String name;
-  	/**
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
   public Person5(String pname) {
-    name = pname;
+	name = 'Connie Huang';
   }
-  	/**
+	/**
 	 * This method should take the string
 	 * input and return its characters rotated
 	 * 2 positions.
@@ -31,19 +31,26 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+		char[] myInput = input.toCharArray();
+		char[] output = new Char[input.length()];
+		for (int i = 0; count< input.length; count++) {
+			output[i+2] = myInput[i];
+
+		}
+	  return output;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
+
 	}
 
 }
